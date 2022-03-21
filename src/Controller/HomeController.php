f5e4eef3e44b631a83f91352ba66b4627e9e3653
choulla <mohammedweb4@gmail.com>
2022-03-21 09:36:48 +0100

    clean code commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,23 +11,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/getfilepath")
-     */
-    public function test(ContainerBagInterface $containerBag){
-        //get file content
-        //$rot = $this->getParameter('kernel.project_dir')."/scripts";
-        //past it in $str
-        //$projectRoot = $containerBag->get('kernel.project_dir');
-       // dd($projectRoot."/scripts/script.sql");
-        //$projectDirAlt = $this->getParameter('kernel.project_dir');
-        //dd($projectDirAlt);
-        //return $containerBag->get('kernel.project_dir')."/scripts/script.sql";
-        dd(
-            file_get_contents(__DIR__."/../../scripts/script.sql")
-        );
-    }
-
-    /**
      * @Route("/",name="home")
      */
     public function index(){
